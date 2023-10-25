0437e6cebe2f4d0272f16df05ab914fa4ac8543d
陶逸敏 <tym386771.jx@chinatelecom.cn>
2023-10-25 17:10:49 +0800

    首次提交

---
+++ assets/guide_doc.md.43e13be0.lean.js
@@ -0,0 +1,1 @@
+import{_ as e,o as t,c as a}from"./chunks/framework.71afdf21.js";const m=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"guide/doc.md","filePath":"guide/doc.md","lastUpdated":1698202846000}'),o={name:"guide/doc.md"};function c(d,r,s,n,i,p){return t(),a("div")}const f=e(o,[["render",c]]);export{m as __pageData,f as default};
---
+++ assets/guide_introduce.md.39ecc1c8.js
@@ -0,0 +1,11 @@
+import{_ as a,o as s,c as n,M as e}from"./chunks/framework.71afdf21.js";const u=JSON.parse('{"title":"项目介绍","description":"","frontmatter":{},"headers":[],"relativePath":"guide/introduce.md","filePath":"guide/introduce.md","lastUpdated":1698202846000}'),t={name:"guide/introduce.md"},p=e(`<h1 id="项目介绍" tabindex="-1">项目介绍 <a class="header-anchor" href="#项目介绍" aria-label="Permalink to &quot;项目介绍&quot;">​</a></h1><h2 id="目录结构" tabindex="-1">目录结构 <a class="header-anchor" href="#目录结构" aria-label="Permalink to &quot;目录结构&quot;">​</a></h2><div class="language-txt"><button title="Copy Code" class="copy"></button><span class="lang">txt</span><pre class="shiki material-theme-palenight"><code><span class="line"><span style="color:#babed8;">.</span></span>
+<span class="line"><span style="color:#babed8;">├─ docs // 文档服务</span></span>
+<span class="line"><span style="color:#babed8;">├─ applications  // 项目目录</span></span>
+<span class="line"><span style="color:#babed8;">│  ├─ app1   // 项目1</span></span>
+<span class="line"><span style="color:#babed8;">│  └─ app2   // 项目2</span></span>
+<span class="line"><span style="color:#babed8;">├─ packages  // 公共模块包</span></span>
+<span class="line"><span style="color:#babed8;">│  ├─ apis   // 接口库</span></span>
+<span class="line"><span style="color:#babed8;">│  ├─ components   // 组件库</span></span>
+<span class="line"><span style="color:#babed8;">│  ├─ http   // 请求库</span></span>
+<span class="line"><span style="color:#babed8;">│  └─ utils  // 工具库</span></span>
+<span class="line"><span style="color:#babed8;">└─ package.json</span></span></code></pre></div>`,3),l=[p];function o(c,i,r,d,b,_){return s(),n("div",null,l)}const m=a(t,[["render",o]]);export{u as __pageData,m as default};
